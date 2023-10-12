2847f7042d7a030fa2187a7d710c33dec7acc4ae
PatiTsi <panagiota.tsitsiouli@dci-student.org>
2023-10-12 21:27:33 +0200

    firts commit

--- cli/query.py
+++ cli/query.py
@@ -9,20 +9,79 @@ for item in warehouse1:
 
 from data import warehouse1, warehouse2
 
-# YOUR CODE STARTS HERE
+# Function to list items by warehouse
+def list_items():
+    print("Items in Warehouse 1:")
+    for item in warehouse1:
+        print(f"- {item}")
+    print("Items in Warehouse 2:")
+    for item in warehouse2:
+        print(f"- {item}")
 
-# Get the user name
+# Function to search for an item and place an order
+def search_and_order():
+    item_name = input("Enter the item name you want to search for: ")
 
-# Greet the user
+    # Search in both warehouses
+    found_in_warehouse1 = item_name in warehouse1
+    found_in_warehouse2 = item_name in warehouse2
 
-# Show the menu and ask to pick a choice
+    total_available = found_in_warehouse1 + found_in_warehouse2
 
-# If they pick 1
-#
-# Else, if they pick 2
-#
-# Else, if they pick 3
-#
-# Else
+    if total_available == 0:
+        print(f"{item_name} is Not in stock.")
+        return
 
-# Thank the user for the visit
+    print(f"Total available: {total_available} item(s)")
+
+     # Determine the location of the item
+    location = []
+    if found_in_warehouse1:
+        location.append("Warehouse 1")
+    if found_in_warehouse2:
+        location.append("Warehouse 2")
+
+    print("Location:", ", ".join(location))
+
+    # Check which warehouse has more of the item
+    if found_in_warehouse1 and found_in_warehouse2:
+        if warehouse1.count(item_name) > warehouse2.count(item_name):
+            print("Warehouse 1 has more.")
+        else:
+            print("Warehouse 2 has more.")
+
+    order_choice = input("Do you want to place an order for this item? (yes/no): ").lower()
+    
+    if order_choice == "yes":
+        desired_amount = int(input(f"How many {item_name} do you want to order? "))
+        if desired_amount <= total_available:
+            print(f"Order placed: {desired_amount} {item_name}")
+        else:
+            max_available = min(found_in_warehouse1, found_in_warehouse2)
+            print(f"Error: Only {max_available} {item_name} available. Do you want to order {max_available} instead? (yes/no)")
+            if input().lower() == "yes":
+                print(f"Order placed: {max_available} {item_name}")
+
+
+
+user_name = input("Please enter your name: ")             # Get the user name
+print(f"Hello, {user_name}!")                             # Greet the user
+
+while True:
+    print("\nMenu:")                                         # Show the menu and ask to pick a choice
+    print("1. List items by warehouse")
+    print("2. Search an item and place an order")
+    print("3. Quit")
+
+    choice = input("Please pick a choice using the numeric values (1/2/3): ")
+
+    if choice == "1":                                               # If they pick 1
+        list_items()
+        break
+    elif choice == "2":                                              # Else, if they pick 2
+        search_and_order()
+    elif choice == "3":
+        print(f"Thank you for visiting, {user_name}!")                  # Else, if they pick 3
+        break
+    else:
+        print("Error: Invalid operation. Please select 1, 2, or 3.")          # Thank the user for the visit      
