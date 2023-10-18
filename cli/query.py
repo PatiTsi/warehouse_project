d59b00d9534733f866604f6d7648051c374120bd
PatiTsi <panagiota.tsitsiouli@dci-student.org>
2023-10-18 16:32:53 +0200

    collections/change on menu option 1

--- cli/query.py
+++ cli/query.py
@@ -1,27 +1,10 @@
-"""Command line interface to query the stock.
+"""Command line interface to query the stock."""
 
-To iterate the source data you can use the following structure:
-
-for item in warehouse1:
-    # Your instructions here.
-    # The `item` name will contain each of the strings (item names) in the list.
-"""
-
-#from data import warehouse1, warehouse2
 from data import stock
 
-# Function to list items by warehouse
-'''
-def list_items():
-    print("Items in Warehouse 1:")
-    for item in warehouse1:
-        print(f"- {item}")
-    print("Items in Warehouse 2:")
-    for item in warehouse2:
-        print(f"- {item}")
-        '''
 
 # Function to list all items and count by warehouse
+
 def list_all_items():
     total_warehouse1 = 0
     total_warehouse2 = 0
@@ -37,28 +20,18 @@ def list_all_items():
     print(f"Total items in Warehouse 2: {total_warehouse2}")
 
 
- # Function to search for items by category (new feature)
-def browse_by_category():
-    category = input("Enter a category to browse: ")
-    found_items = [item for item in stock if category.lower() in item['category'].lower()]
-
-    if found_items:
-        for item in found_items:
-            days_in_stock = (datetime.now() - item['date_of_stock']).days
-            print(f"{item['state']} {item['category']} (Warehouse {item['warehouse']}), Days in stock: {days_in_stock}")
-    else:
-        print(f"No items found in the category: {category}")   
-
+   
 
 # Function to search for an item and place an order
 def search_and_order():
     item_name = input("Enter the item name you want to search for: ")
 
-    # Search in both warehouses
-    found_in_warehouse1 = item_name in stock
-    found_in_warehouse2 = item_name in stock
 
-    total_available = found_in_warehouse1 + found_in_warehouse2
+    # Search in both warehouses
+    found_in_warehouse1 = [item for item in stock if item_name in f"{item['state']} {item['category']}" and item['warehouse'] == 1]
+    found_in_warehouse2 = [item for item in stock if item_name in f"{item['state']} {item['category']}" and item['warehouse'] == 2]
+
+    total_available = len(found_in_warehouse1) + len(found_in_warehouse2)
 
     if total_available == 0:
         print(f"{item_name} is Not in stock.")
@@ -83,13 +56,13 @@ def search_and_order():
             print("Warehouse 2 has more.")
 
     order_choice = input("Do you want to place an order for this item? (yes/no): ").lower()
-    
+
     if order_choice == "yes":
         desired_amount = int(input(f"How many {item_name} do you want to order? "))
         if desired_amount <= total_available:
             print(f"Order placed: {desired_amount} {item_name}")
         else:
-            max_available = min(found_in_warehouse1, found_in_warehouse2)
+            max_available = min(len(found_in_warehouse1) , len(found_in_warehouse2))
             print(f"Error: Only {max_available} {item_name} available. Do you want to order {max_available} instead? (yes/no)")
             if input().lower() == "yes":
                 print(f"Order placed: {max_available} {item_name}")
@@ -103,13 +76,13 @@ while True:
     print("\nMenu:")                                         # Show the menu and ask to pick a choice
     print("1. List items by warehouse")
     print("2. Search an item and place an order")
-    print("3. Browse items by category")
-    print("4. Quit")
+    print("3. Quit")
 
     choice = input("Please pick a choice using the numeric values (1/2/3): ")
 
     if choice == "1":                                               # If they pick 1
         list_all_items()
+        print(f"Thank you for visiting, {user_name}!")
         break
     elif choice == "2":                                              # Else, if they pick 2
         search_and_order()
